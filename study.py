6c4a2383c9d3c751684bb6db03debed395526cdb
Manuel <manuel.roeder@fhws.de>
2023-03-29 15:01:59 +0200

    proto_fs: Patch study script
    Make script more flexible by taking care of all
    clients.

--- study.py
+++ study.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import uuid
 from argparse import ArgumentParser
 
@@ -30,57 +29,59 @@ def run_server_pretrain_on_domain(server_idx, dataset, subdir, net):
     print("Server pretraining finished")
 
 
-def run_server_client_study(server_idx, client_id_1, client_id_2, dataset, subdir, K=5, adaptation_enabled=True, net="resnet34"):
+def run_server_client_study(server_idx, client_ids, dataset, subdir, K=5, adaptation_enabled=True, net="resnet34"):
     if not os.path.exists(subdir):
         os.makedirs(subdir)
     if adaptation_enabled:
         adaptation = "end_2_end"
     else:
         adaptation = "none"
+
     # unique run id
     id = uuid.uuid4()
-    source_file = os.path.join(subdir, str(id) + "_out_server_source_" + dataset.get_domain_names()[server_idx] + "_K_" + str(K) +".txt")
-    c1_file = os.path.join(subdir, str(id) + "_out_client_source_" + dataset.get_domain_names()[server_idx] + "_target_" + dataset.get_domain_names()[client_id_1] + "_adapt_" + adaptation + "_K_" + str(K) + ".txt")
-    c2_file = os.path.join(subdir, str(id) + "_out_client_source_" + dataset.get_domain_names()[server_idx] + "_target_" + dataset.get_domain_names()[client_id_2] + "_adapt_" + adaptation + "_K_" + str(K) + ".txt")
-    server_log = open(source_file, 'w')
-    c1_log = open(c1_file, 'w')
-    c2_log = open(c2_file, 'w')
+
     try:
-        processes = []
-        server_call = "python server.py --config_file=config/server_config.yaml --dataset=" + dataset.get_dataset_name() + " --subdomain_id=" + str(server_idx) + " --adaptation_type=" + adaptation + " --net=" + net + " --default_root_dir=" + subdir + " --K=" + str(K)
+        source_file = os.path.join(subdir, str(id) + "_out_server_source_" + dataset.get_domain_names()[server_idx] + "_K_" + str(K) +".txt")
+        server_log = open(source_file, 'w')
+        # start the server
+        server_call = "python server.py --config_file=config/server_config.yaml --dataset=" + dataset.get_dataset_name() + " --subdomain_id=" + str(
+            server_idx) + " --adaptation_type=" + adaptation + " --net=" + net + " --default_root_dir=" + subdir + " --K=" + str(
+            K)
         print(server_call)
         server_job = subprocess.Popen(server_call.split(), stdout=server_log, stderr=subprocess.STDOUT, shell=False)
         print("Server boot, waiting...")
         time.sleep(10)
-        client_1_call = "python client.py --config_file=config/client_config.yaml --dataset=" + dataset.get_dataset_name() + " --client_id=" + str(client_id_1) + " --subdomain_id=" + str(server_idx) + " --net=" + net + " --default_root_dir=" + subdir
-        print(client_1_call)
-        c1_job = subprocess.Popen(client_1_call.split(), stdout=c1_log, stderr=subprocess.STDOUT, shell=False)
-        print("Client 1 boot done")
-        time.sleep(5)
-        client_2_call = "python client.py --config_file=config/client_config.yaml --dataset=" + dataset.get_dataset_name() + " --client_id=" + str(client_id_2) + " --subdomain_id=" + str(server_idx) + " --net=" + net + " --default_root_dir=" + subdir
-        print(client_2_call)
-        c2_job = subprocess.Popen(client_2_call.split(), stdout=c2_log, stderr=subprocess.STDOUT, shell=False)
 
-        processes.append(server_job)
-        processes.append(c1_job)
-        processes.append(c2_job)
+        # client logic
+        study_tracker = list() # list with client id, log file path and job
+        for client_id in client_ids:
+            # file path
+            fp = os.path.join(subdir, str(id) + "_out_client_source_" + dataset.get_domain_names()[server_idx] + "_target_" + dataset.get_domain_names()[client_id] + "_adapt_" + adaptation + "_K_" + str(K) + ".txt")
+            log = open(fp, 'w')
+            client_call = "python client.py --config_file=config/client_config.yaml --dataset=" + dataset.get_dataset_name() + " --client_id=" + str(client_id) + " --subdomain_id=" + str(server_idx) + " --net=" + net + " --default_root_dir=" + subdir
+            print(client_call)
+            job = subprocess.Popen(client_call.split(), stdout=log, stderr=subprocess.STDOUT, shell=False)
+            study_tracker.append((client_id, (log, job)))
+            time.sleep(5)
 
-        print("Client 2 boot done, waiting for study to shut down...")
-        for p in processes:
-            if p.wait() != 0:
+        print("[STUDY] All clients booted, study running ...")
+        for client in study_tracker:
+            if client[1][1].wait() != 0:
                 print("[STUDY] There was an error")
 
     except RuntimeError as err:
         print(repr(err))
         torch.cuda.empty_cache()
+        # close the log files
+        for client in study_tracker:
+            client[1][0].close()
+        return "Study " + str(id) + " failed"
 
-    # close all logs
-    server_log.close()
-    c1_log.close()
-    c2_log.close()
+    # close the log files
+    for client in study_tracker:
+        client[1][0].close()
 
     return "Study " + str(id) + " finished"
-
 
 
 def main():
@@ -107,20 +108,22 @@ def main():
         dataset = DomainNetDataModule
         num_classes = 345
 
+
+    # how many subdomains exist
     subdomain_count = len(dataset.get_domain_names())
+    # subdomain id list from 0 to subdomain_count
+    subdomain_ids = list(range(0, subdomain_count))
+    num_clients = subdomain_count - 1
+
+    # parameter K for this study
     K_list = [3, 5, 10]
     folder = os.path.join(args.subdir, dataset.get_dataset_name())
 
     for server_idx in range(subdomain_count):
-        if server_idx == 0:
-            client_id_1 = 1
-            client_id_2 = 2
-        elif server_idx == 1:
-            client_id_1 = 0
-            client_id_2 = 2
-        elif server_idx == 2:
-            client_id_1 = 0
-            client_id_2 = 1
+        # get the full list of available domain ids
+        c_ids = subdomain_ids.copy()
+        # remove the server id from the list
+        c_ids.remove(server_idx)
 
         # check if server pretrained files exist
         net = args.net
@@ -142,8 +145,7 @@ def main():
 
         # no adaptation run
         run_server_client_study(server_idx,
-                                client_id_1,
-                                client_id_2,
+                                c_ids,
                                 dataset,
                                 K=3, #wont be used without adaptation
                                 subdir=run_folder,
@@ -151,16 +153,13 @@ def main():
                                 net=args.net)
         for K in K_list:
             run_server_client_study(server_idx,
-                                    client_id_1,
-                                    client_id_2,
+                                    c_ids,
                                     dataset,
                                     K=K,
                                     subdir=run_folder,
                                     adaptation_enabled=True)
 
-
     print("Finished study")
-
 
 
 if __name__ == "__main__":
